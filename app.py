33be3b9e7fcee789f76375b139fb381026b4bb5e
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-09-19 10:56:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,19 +41,23 @@ headers = {
 upload_endpoint = 'https://api.assemblyai.com/v2/upload'
 transcription_endpoint = "https://api.assemblyai.com/v2/transcript"
 
-# DESIGN implement changes to the standard streamlit UI/UX
-st.set_page_config(page_title="streamlit_audio_recorder")
-# Design move app further up and remove top padding
-st.markdown('''<style>.css-1egvi7u {margin-top: -3rem;}</style>''',
-    unsafe_allow_html=True)
-# Design change st.Audio to fixed height of 45 pixels
-st.markdown('''<style>.stAudio {height: 45px;}</style>''',
-    unsafe_allow_html=True)
-# Design change hyperlink href link color
-st.markdown('''<style>.css-v37k9u a {color: #ff4c4b;}</style>''',
-    unsafe_allow_html=True)  # darkmode
-st.markdown('''<style>.css-nlntq9 a {color: #ff4c4b;}</style>''',
-    unsafe_allow_html=True)  # lightmode
+# STREAMLIT AUDIO RECORDER Instance
+val = st_audiorec()
+# web component returns arraybuffer from WAV-blob
+st.write('Audio data received in the Python backend will appear below this message ...')
+
+if isinstance(val, dict):  # retrieve audio data
+    with st.spinner('retrieving audio-recording...'):
+        ind, val = zip(*val['arr'].items())
+        ind = np.array(ind, dtype=int)  # convert to np array
+        val = np.array(val)             # convert to np array
+        sorted_ints = val[ind]
+        stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
+        wav_bytes = stream.read()
+
+        # wav_bytes contains audio data in format to be further processed
+        # display audio data as received on the Python side
+    st.audio(wav_bytes, format='audio/wav')
 
 def upload_to_assemblyai(file_path):
 
