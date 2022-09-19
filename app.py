9267be51e7d7b66d1a26fbb40a9d3be353ea0fb6
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-09-19 10:05:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ from nltk.corpus import stopwords
 import re
 import string
 
+# disable warnings
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+st.title('Job Recommender System')
+st.subheader("Navigate to side bar to see project info")
+st.subheader("See below for options")
+
+
+hide_streamlit_style = '''
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+'''
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
 inp = st.text_area(
         "Write your text here!", max_chars=2000, height=150
     )
