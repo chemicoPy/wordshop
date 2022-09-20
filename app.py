5b15c4a7951ec320e21233f17589371142810331
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-09-20 10:23:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def main():
 
         # wav_bytes contains audio data in format to be further processed
         # display audio data as received on the Python side
-       st.audio(wav_bytes, format='audio/wav')
+       audio_data = st.audio(wav_bytes, format='audio/wav')
             
    file_path = "input.wav"
 
