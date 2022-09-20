d116163dd29663cc176da0b75a3680fc9178e041
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-09-20 10:28:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,24 +100,6 @@ def main():
    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
 
    #st_audiorec()
-
-
-   val = st_audiorec()
-    # web component returns arraybuffer from WAV-blob
-   st.write('Audio data received in the Python backend will appear below this message ...')
-
-   if isinstance(val, dict):  # retrieve audio data
-       with st.spinner('retrieving audio-recording...'):
-           ind, val = zip(*val['arr'].items())
-           ind = np.array(ind, dtype=int)  # convert to np array
-           val = np.array(val)             # convert to np array
-           sorted_ints = val[ind]
-           stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
-           wav_bytes = stream.read()
-
-        # wav_bytes contains audio data in format to be further processed
-        # display audio data as received on the Python side
-   audio_data = st.audio(wav_bytes, format='audio/wav')
             
    file_path = "input.wav"
 
