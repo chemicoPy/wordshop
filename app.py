2c43ff6af502b9da2d1baac346634e188e453675
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-09-20 10:15:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -95,13 +95,22 @@ def call_gpt3(prompt):
 
 def main():
 
-    st.title("Using voice control")
-            
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
-    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
+   val = st_audiorec()
+    # web component returns arraybuffer from WAV-blob
+    st.write('Audio data received in the Python backend will appear below this message ...')
 
-    st_audiorec()
+    if isinstance(val, dict):  # retrieve audio data
+        with st.spinner('retrieving audio-recording...'):
+            ind, val = zip(*val['arr'].items())
+            ind = np.array(ind, dtype=int)  # convert to np array
+            val = np.array(val)             # convert to np array
+            sorted_ints = val[ind]
+            stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
+            wav_bytes = stream.read()
+
+        # wav_bytes contains audio data in format to be further processed
+        # display audio data as received on the Python side
+        st.audio(wav_bytes, format='audio/wav')
             
     file_path = "input.wav"
 
