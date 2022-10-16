e077bb77395e4283faaa3a0e9e892b7e25e40193
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-10-17 00:35:24 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -15,5 +15,4 @@ WAVE_OUTPUT_FILE = os.path.join(RECORDING_DIR, "recorded.wav")
 INPUT_DEVICE = 0
 MAX_INPUT_CHANNELS = 1  # Max input channels
 DEFAULT_SAMPLE_RATE = 44100   # Default sample rate of microphone or recording device
-DURATION = 3   # 3 seconds
 CHUNK_SIZE = 1024
