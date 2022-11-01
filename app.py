57e19f9b99334fb8043e7d7020b10e2f5889040d
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-11-01 01:06:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,8 @@ def main():
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
     st_audiorec = components.declare_component("st_audiorec", path=build_dir)
-
+    
+    st.write(parent_dir)
     #st_audiorec()
     val = st_audiorec()
     
