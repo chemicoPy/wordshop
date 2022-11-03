22bd67b4bb1b52537631a3c8bd5275ccd1aa00d3
Victor Ogunjobi <66210738+chemicoPy@users.noreply.github.com>
2022-11-03 03:53:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_option('deprecation.showfileUploaderEncoding', False)
    
 assembly_auth_key = st.secrets["assembly_apikey"]
-openai.api_key = st.secrets["openai_apikey"]
+openai.api_key = os.getenv("OPENAI_APIKEY")
 
 headers = {
     'authorization': assembly_auth_key, 
