e254dcee238dad854b1d7d8c9ba67b6908f9c245
Victor Ogunjobi <victorogunjobi8@gmail.com>
2023-03-06 12:18:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,31 +16,9 @@ import math
 from pathlib import Path
 
 # Desiging & implementing changes to the standard streamlit UI/UX
-st.set_page_config(page_icon="img/icon_2.jpg")    #Logo
-st.markdown('''<style>.css-1egvi7u {margin-top: -4rem;}</style>''',
-    unsafe_allow_html=True)
-# Design change hyperlink href link color
-st.markdown('''<style>.css-znku1x a {color: #9d03fc;}</style>''',
-    unsafe_allow_html=True)  # darkmode
-st.markdown('''<style>.css-znku1x a {color: #9d03fc;}</style>''',
-    unsafe_allow_html=True)  # lightmode
-# Design change height of text input fields headers
-st.markdown('''<style>.css-qrbaxs {min-height: 0.0rem;}</style>''',
-    unsafe_allow_html=True)
-# Design change spinner color to primary color
-st.markdown('''<style>.stSpinner > div > div {border-top-color: #9d03fc;}</style>''',
-    unsafe_allow_html=True)
-# Design change min height of text input box
-st.markdown('''<style>.css-15tx938{min-height: 0.0rem;}</style>''',
-    unsafe_allow_html=True)
+#st.set_page_config(page_icon="img/icon_2.jpg")    #Logo
 
-# Design hide top header line
-hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
-st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
-# Design hide "made with streamlit" footer menu area
-hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
-                        footer {visibility: hidden;}</style>"""
-st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
+st.set_page_config(page_title='Market Monitor', page_icon="img/icon_2.jpg", layout='wide', initial_sidebar_state='expanded')
 
 # disable warnings
 st.set_option('deprecation.showPyplotGlobalUse', False)
